12ae86774d9778394a102b104bdeb57e48d1f052
Hassan <hassan.abbp@gmail.com>
2018-12-04 13:55:09 +0330

    Fix non-alphabetic characters problem

--- finglish2farsi.py
+++ finglish2farsi.py
@@ -4,6 +4,21 @@ from config import TOKEN
 from time import sleep
 from finglish import f2p
 
+def f2f(text) :
+    std_text = ''
+    for char in text:
+        if(not char.isalpha()):
+            std_text += ' %c '%char
+        else:
+            std_text += char
+
+    farsi_text = ''
+    for word in std_text.split():
+        farsi_text += ' ' + f2p(word)
+
+    return farsi_text
+
+
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     if chat_type == u'private':
@@ -11,7 +26,7 @@ def handle(msg):
             if msg['text'] == '/start' :
                 bot.sendMessage(chat_id, '*WELCOME* 🙂\nI translate Finglish to Farsi', parse_mode='Markdown')
             else:
-                bot.sendMessage(chat_id, f2p(msg['text']))
+                bot.sendMessage(chat_id, f2f(msg['text']))
 
 
 bot = telepot.Bot(TOKEN)
